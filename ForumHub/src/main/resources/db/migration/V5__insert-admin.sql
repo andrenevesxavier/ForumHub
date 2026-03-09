2e5f274bf872a7455801f9545b35554229d84a4b
andrenevesxavier <andrenevesxavier276@gmail.com>
2026-03-09 16:42:32 -0300

    feat : migration V5

---
+++ ForumHub/src/main/resources/db/migration/V5__insert-admin.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO usuarios (nome, email, senha, ativo)
+VALUES ('Admin', 'admin@forumhub.com', '$2a$10$oxBP66P7ZkFbPDz1CEVC4uoF3I4wLy3GYAlFWMYnZh5.zZGYbdq3K', true);
+
+INSERT INTO usuario_perfis (usuario_id, perfil_id)
+VALUES (1, 1);
